65efb8f8600481c2e6fbff7999341189224eba9b
Kiet Nguyen <95566481+alumica@users.noreply.github.com>
2023-05-07 23:47:51 +0700

    Delete restaurant directory

--- restaurant/restaurant/src/assets/img/index.jsx
+++
@@ -1,8 +0,0 @@
-const images ={
-    backgroundheader: require("./backgroundheader.png"),
-    salad: require("./salad.jpg"),
-    login:require("./login.jpg"),
-    googleicon: require("./google.png"),
-    facebookicon: require("../img/facebook.png")
-}
-export default images;
