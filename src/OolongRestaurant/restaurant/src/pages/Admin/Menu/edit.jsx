fb7619288756e2f9b944d132b7970b40aa5a3a16
Kiet Nguyen <alumica812@gmail.com>
2023-05-07 16:30:40 +0700

    Update file.Fix

--- src/OolongRestaurant/restaurant/src/pages/Admin/Menu/edit.jsx
+++ src/OolongRestaurant/restaurant/src/pages/Admin/Menu/edit.jsx
@@ -90,7 +90,7 @@ const EditMenu = () => {
                         </label>
                     </div>
                     <div className="md:w-2/3">
-                        <input name="urlSlug" title="Url Slug" value={menu.price || ''}
+                        <input name="urlSlug" title="Url Slug" value={menu.urlSlug || ''}
                             onChange={e => setMenu({...menu, urlSlug: e.target.value
                                 })}
                             className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" id="inline-password" type="text"/>
--- src/OolongRestaurant/restaurant/src/pages/Contact/index.jsx
+++ src/OolongRestaurant/restaurant/src/pages/Contact/index.jsx
@@ -38,21 +38,21 @@ function Contact() {
           <input type='hidden' name='id' value={contact.id}/>
           <div>
               <label htmlFor="email" className="block mb-2 text-base font-medium text-white">Email</label>
-              <input name='email' onChange={e => setContact({...contact, email: e.target.value})} type="email" id="email" className=" shadow-base p-3 bg-white/10 border border-white text-white text-base rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="" required />
+              <input name='email' onChange={e => setContact({...contact, email: e.target.value})} type="email" id="email" className=" shadow-base p-3 bg-white/10 border border-white text-white text-base rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full" placeholder="" required />
           </div>
           <div>
               <label htmlFor="fullName" className="block mb-2 text-base font-medium text-white">Full Name</label>
-              <input name='fullName' onChange={e => setContact({...contact, fullName: e.target.value})} type="text" id="fullName" className=" shadow-base p-3 bg-white/10 border border-white text-white text-base rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="" required />
+              <input name='fullName' onChange={e => setContact({...contact, fullName: e.target.value})} type="text" id="fullName" className=" shadow-base p-3 bg-white/10 border border-white text-white text-base rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full" placeholder="" required />
           </div>
           <div>
               <label htmlFor="subject" className="block mb-2 text-base font-medium text-white">Subject</label>
-              <input name='subject' onChange={e => setContact({...contact, subject: e.target.value})} type="text" id="subject" className="block p-3 w-full text-base text-white bg-white/10 rounded-lg border border-gray-300 shadow-base focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="" required />
+              <input name='subject' onChange={e => setContact({...contact, subject: e.target.value})} type="text" id="subject" className="block p-3 w-full text-base text-white bg-white/10 rounded-lg border border-gray-300 shadow-base focus:ring-primary-500 focus:border-primary-500" placeholder="" required />
           </div>
           <div className="sm:col-span-2">
               <label htmlFor="message" className="block mb-2 text-base font-medium text-white dark:text-gray-400">Your message</label>
-              <textarea name='description' onChange={e => setContact({...contact, description: e.target.value})} id="message" rows="6" className="block p-2.5 w-full text-base text-white bg-white/10 rounded-lg shadow-base border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder=""></textarea>
+              <textarea name='description' onChange={e => setContact({...contact, description: e.target.value})} id="message" rows="6" className="block p-2.5 w-full text-base text-white bg-white/10 rounded-lg shadow-base border border-gray-300 focus:ring-primary-500 focus:border-primary-500 " placeholder=""></textarea>
           </div>
-          <button type="submit" className="py-3 px-5 text-base font-medium text-center bg-primary-black text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
+          <button type="submit" className="py-3 px-5 text-base font-medium text-center bg-primary-black text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 ">Send message</button>
       </form>
   </div>
 </section>
